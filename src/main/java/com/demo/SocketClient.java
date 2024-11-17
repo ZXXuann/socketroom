3d94e49f3984530d24beb1282ce97454278e0bb6
wandefulha <1139746716@qq.com>
2024-11-17 12:16:56 +0800

    feat:更新客户端

--- src/main/java/com/demo/SocketClient.java
+++ src/main/java/com/demo/SocketClient.java
@@ -1,9 +1,6 @@
 package com.demo;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -16,7 +13,7 @@ public class SocketClient {
             //获取socket的输出流
             OutputStream os=socket.getOutputStream();
             OutputStreamWriter osw=new OutputStreamWriter(os);
-
+            BufferedWriter bw=new BufferedWriter(osw);
             System.out.println("请输入内容");
 
             //创建一个发送数据的线程
@@ -25,12 +22,36 @@ public class SocketClient {
                     Scanner sc=new Scanner(System.in);
                     //读取文字
                     String input=sc.nextLine();
-                    InputStream inputStream=socket.getInputStream();
+                    try {
+                        //输出文字
+                        bw.write(input);
+                        bw.flush();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        break;
+                    }
 
                 }
-            })
+            }).start();
+
+            //创建一个接收数据的线程
+            InputStream is=socket.getInputStream();
+            InputStreamReader isr=new InputStreamReader(is);
+            BufferedReader br=new BufferedReader(isr);
+            new Thread(()->{
+                while(true){
+                    try {
+                        String message=br.readLine();
+                        System.out.println("收到服务端的消息:"+message);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        break;
+                    }
+
+                }
+            }).start();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
 
     }
